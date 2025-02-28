ab4c38a15cd1c941c0364f9151f1df9fb0b81ee6
morshed-901 <138610183+morshed-901@users.noreply.github.com>
2025-02-28 11:39:21 +0600

    Update bdsex.js

--- scripts/commands/bdsex.js
+++ scripts/commands/bdsex.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: "1.0.0",
   permission: 2,
   credits: "Nayan",
-  description: "Sex pic",
+  description: "Morshed",
   prefix: true, 
   category: "user", 
   usages: "Morshed",
@@ -26,6 +26,6 @@ const fs = global.nodemodule["fs-extra"];
     "https://i.postimg.cc/Mp4myjGx/556-contact-01749889097.jpg",
 
     "https://i.postimg.cc/rm2GHXWP/images-2022-08-16-T112453-202.jpg", ];
-   var callback = () => api.sendMessage({body:`ভদ্র ছেলে😋+\nSố ảnh: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+   var callback = () => api.sendMessage({body:`ভদ্র ছেলে😋+\n লজ্জাবতী ছেলে: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
