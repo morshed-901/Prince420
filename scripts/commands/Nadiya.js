1fa00b59ce0eb11914a3ca55d6c5e48b24fe69af
morshed-901 <138610183+morshed-901@users.noreply.github.com>
2025-03-01 09:21:32 +0600

    Create Nadiya.js

---
+++ scripts/commands/Nadiya.js
@@ -0,0 +1,159 @@
+module.exports.config = {
+
+name: "hadiya",
+
+version: "1.0.0",
+
+permssion: 0,
+
+credits: "EMon-BHai",
+
+prefix:true,
+
+description: "auto reply to hadiya",
+
+category: "noprefix",
+
+usages: "hadiya",
+
+cooldowns: 5,
+
+dependencies: {
+
+"request":"",
+
+"fs-extra":"",
+
+"axios":""
+
+}
+
+};
+
+module.exports.handleEvent = async ({ api, event, Threads,Users}) => {
+
+var id = event.senderID;
+
+var name = await Users.getNameUser(event.senderID);
+
+if (event.body.indexOf("Bikash")==0 || (event.body.indexOf("বিকাশ")==0) || event.body.indexOf("Bikash")==0 ||
+
+event.body.indexOf("বিকাশ")==0 ||
+
+event.body.indexOf("নগদ")==0 ||
+
+event.body.indexOf("Nagad")==0 ||
+
+event.body.indexOf("রকেট")==0 ||
+
+event.body.indexOf("Rocket")==0 ||
+
+event.body.indexOf("Nambar")==0 ||
+
+event.body.indexOf("নাম্বার")==0) {
+
+const axios = global.nodemodule["axios"];
+
+const request = global.nodemodule["request"];
+
+const fs = global.nodemodule["fs-extra"];
+
+var link = [
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+"https://i.imgur.com/rIowT2K.jpeg",
+
+];
+
+var callback = () => api.sendMessage({body:`╔═.✵.═══════════╗\n 01615101797-!!🖤💫\n╚═══════════.✵.═╝\n\n✿🦋༎প্রি্ঁয়্ঁ গ্রুপ্ঁ মে্ঁম্ঁবা্ঁর্ঁ ${name}༎✨🧡\n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`,attachment: fs.createReadStream(__dirname + "/cache/emon.jpeg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/emon.jpeg"), event.messageID);
+
+const timeStart = Date.now();
+
+const dcm = process.uptime(); 
+
+var anh = Math.floor(dcm / (60 * 60));
+
+var la = Math.floor((dcm % (60 * 60)) / 60);
+
+var vt = Math.floor(dcm % 60);
+
+const PREFIX = config.PREFIX;
+
+return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/emon.jpeg")).on("close",() => callback());
+
+}
+
+module.exports.languages = {
+
+"vi": {
+
+"on": "Use it the wrong way and then complain",
+
+"off": "Stupid student, used it the wrong way",
+
+"successText": `🧠`,
+
+},
+
+"en": {
+
+"on": "on",
+
+"off": "off",
+
+"successText": "success!",
+
+}
+
+}
+
+module.exports.run = async ({ event, api, Threads, getText }) => {
+
+let { threadID, messageID } = event;
+
+let data = (await Threads.getData(threadID)).data;
+
+if (typeof data["salam"] == "undefined" || data["salam"] == true) data["salam"] = false;
+
+else data["salam"] = true;
+
+await Threads.setData(threadID, {
+
+data
+
+});
+
+global.data.threadData.set(threadID, data);
+
+api.sendMessage(`${(data["salam"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+
+} 
+
+}
+
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+
+};
