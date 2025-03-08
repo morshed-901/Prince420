dc9b45cbe755ed32a7b16045c2191ea3b02d11a1
morshed-901 <138610183+morshed-901@users.noreply.github.com>
2025-03-08 12:43:36 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "গ্রুপ থেকে না বলে চলে যাওয়া ব্যক্তিদের আর এড করা যাবে না 🤢" : "ইসলামিয়া ফাউন্ডেশনের কৃতপক্ষ";
 	const path = join(__dirname, "nayan", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "ইসলামিয়া ফাউন্ডেশন এর নিয়ম না মানার কারণে {name} কে গ্রুপে থেকে বহিষ্কার করা হলো  .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "ইসলামিয়া ফাউন্ডেশন এর নিয়ম না মানার কারণে {name} কে গ্রুপ থেকে বহিষ্কার করা হলো  .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
